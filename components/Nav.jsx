ab6e91247edc169a1086f79edeb284b352d84e72
Sushmoy Roy <sushmoyr@gmail.com>
2023-05-07 16:19:51 +0600

    feat(page.jsx): add Home component with Feed component feat(components): add Feed, Form, and Nav components
    feat(Provider.jsx): add Provider component with empty div

---
+++ components/Nav.jsx
@@ -0,0 +1,36 @@
+"use client";
+
+import Link from 'next/link';
+import Image from 'next/image'
+import {signOut} from "next-auth/react";
+
+
+const Nav = () => {
+    const isUserLoggedIn = true;
+    return (
+        <nav className="flex-between w-full mb-16 pt-3">
+            <Link href={"/"} className={"flex gap-2 flex-center"}>
+                <Image src={"/assets/images/logo.svg"} alt={"logo"} width={30} height={30}
+                       className={"object-contain"}/>
+                <p className={"logo_text"}>Promptopia</p>
+            </Link>
+
+            {/* Mobile Navigation */}
+            <div className="sm:flex hidden">
+                {
+                    isUserLoggedIn ?
+                        <div className={"flex gap-3 md:gap-5"}>
+                            <Link href={"/create-prompt"} className={"black_btn"}>Create Post</Link>
+                            <button type={"button"} onClick=signOut className={"outline_btn"}>
+                                Sign Out
+                            </button>
+                            <Link href={"/profile"}/>
+                        </div> : <></>
+                }
+            </div>
+
+        </nav>
+    );
+};
+
+export default Nav;
